66bf27ed6f680d3219d1e4e8bf2dac07ba07639d
PhylisMercy <phylis.mercy42@gmail.com>
2023-01-09 16:46:17 +0000

    an empty class BaseGeometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3                                                                                                              
+# -*- coding: utf-8 -*-                                                                                                         
+"""                                                                                                                             
+Created on Monday January 9 6:52:37 2023                                                                                        
+@author: Phylis Mercy                                                                                                           
+""" 
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
