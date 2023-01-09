ece8364de106708a76f6c049885ae79bf7521765
PhylisMercy <phylis.mercy42@gmail.com>
2023-01-09 16:49:01 +0000

    a class BaseGeometry,Public instance method: def area(self): that raises an Exception with the message area() is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3                                                                                                              
+# -*- coding: utf-8 -*-                                                                                                         
+"""                                                                                                                             
+Created on Monday January 9 6:52:37 2023                                                                                        
+@author: Phylis Mercy                                                                                                           
+""" 
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
+
+    def area(self):
+        """
+        Public instance method that calculates the area
+        Raises:
+            Exception if area is not imlemented
+        """
+        raise Exception("area() is not implemented")
