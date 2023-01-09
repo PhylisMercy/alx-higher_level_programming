65d1c07cd04cc59bc52716adbf5ce73a2baac5b5
PhylisMercy <phylis.mercy42@gmail.com>
2023-01-09 15:49:15 +0000

    a function that returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Monday January 9 6:47:37 2023
+@author: Phylis Mercy
+"""
+
+
+def lookup(obj):
+    """
+    Returns all objects in an objects dictionary as a list
+    """
+    return dir(obj)
