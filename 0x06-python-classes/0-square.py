f02d3de99052791068f38e33105d68306a702c16
PhylisMercy <phylis.mercy42@gmail.com>
2023-01-11 16:46:52 +0000

    an empty class Square that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wednesday January 11 19:43 2023
+@author: Phylis Mercy
+"""
+
+
+class Square:
+    """Empty class Square that defines a square
+    Attributes:
+        empty
+    """
+    pass
