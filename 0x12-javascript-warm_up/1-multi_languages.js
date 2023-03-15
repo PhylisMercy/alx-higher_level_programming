4e9bda05bb75e4db47fc47b2f39944a9a5ee2aaf
PhylisMercy <phylis.mercy42@gmail.com>
2023-03-15 06:26:45 -0700

    implement code style

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const language1 = "C is fun";
-const language2 = "Python is cool";
-const language3 = "JavaScript is amazing";
+const language1 = 'C is fun';
+const language2 = 'Python is cool';
+const language3 = 'JavaScript is amazing';
 
 console.log(language1);
 console.log(language2);
