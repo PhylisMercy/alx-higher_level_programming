826937e52113c2e32b0f599ff6c6b67a23420e88
PhylisMercy <phylis.mercy42@gmail.com>
2023-04-05 09:36:30 -0700

    a Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed April 5 07:35 2023
+@author: Phylis Mercy
+"""
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        with urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
+
